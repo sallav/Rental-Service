feb82b99ef9fec25b57722929ef9cf2b020e4400
sallav <salla_mari@hotmail.com>
2022-05-24 23:11:34 +0300

    Added port

--- install.php
+++ install.php
@@ -8,7 +8,7 @@
 require "config.php";
 
 try {
-    $connection = new PDO("mysql:host=$host;port=3307;", $username, $password, $options);
+    $connection = new PDO("mysql:host=$host;port=$port;", $username, $password, $options);
     // Set PDO error mode to exception
     $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = file_get_contents("data/init.sql");
